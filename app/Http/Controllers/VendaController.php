4185927eba9e7cc58caea7ca10fb0af2ede961f7
Jesyka <jessycabatista57@gmail.com>
2024-01-28 23:22:00 -0300

    atualização da documentação

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -10,14 +10,61 @@ use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Throwable;
 
+/**
+ * @QA\Server(url="http://localhost/api"),
+ * @OA\Info(title="Docker Example", version="0.0.0.1"),
+ */
 class VendaController extends Controller
 {
-    //
+    /**
+     * @OA\Get(
+     *     path="/api/produtos",
+     *     tags={"Produtos"},
+     *     summary="Obtém a lista de todos os produtos",
+     *     @OA\Response(response=200, description="Operação executada com sucesso"),
+     *     @OA\Response(response=401, description="Não autenticado"),
+     *     @OA\Response(response=403, description="Proibido"),
+     * )
+     */
     public function listarProdutosDisponiveis()
     {
         $celulares = Celular::all();
         return response()->json($celulares);
     }
+    /**
+     * @OA\Post(
+     *     path="/api/vendas",
+     *     tags={"Produtos"},
+     *     summary="Criação de uma nova venda",
+     *     @OA\Parameter(
+    *      description="ID da venda",
+    *      in="path",
+    *      name="id_venda",
+    *      required=true,
+    *      @OA\Schema(type="int"),
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="JSON para a criação da venda",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="products",
+     *                 type="array",
+     *                 @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(property="product_id", type="int"),
+     *                     @OA\Property(property="nome", type="string"),
+     *                     @OA\Property(property="price", type="float"),
+     *                     @OA\Property(property="amount", type="int")
+     *                 )
+     *             )
+     *         ),
+     *     ),
+     *     @OA\Response(response=400, description="A venda já existe"),
+     *     @OA\Response(response=422, description="Dados não processados")
+     * )
+     */
 
     public function cadastrarVenda(Request $request)
     {
@@ -26,6 +73,7 @@ class VendaController extends Controller
             $venda_existente = Venda::find($request->id_venda);
             if($request->id_venda === $venda_existente)
                 throw new Exception("A venda já existe", 400);
+
             $venda = new Venda();
             $venda->venda_id = $request->id_venda;
             $venda->save();
@@ -70,7 +118,7 @@ class VendaController extends Controller
             //return response()->json(['error' => 'Erro ao vender produto: ' . $e->getMessage()], 500);
         //}
     }
-    public function editarVenda($id,Request $request){
+    public function editarVenda($id,Request $request){  
         $venda = VendaProduto::where('venda_id',$id);
         if($venda){
             foreach ($request->products as $product)
@@ -107,18 +155,53 @@ class VendaController extends Controller
             return response()->json(['error' => 'Erro ao adicionar o produto'], 400);
         }
     }
+    /**
+     * @OA\Get(
+     *     path="/api/vendas",
+     *     tags={"Produtos"},
+     *     summary="Obtém a lista de todas as vendas realizadas",
+     *     @OA\Response(response=200, description="Operação executada com sucesso"),
+     *     @OA\Response(response=401, description="Não autenticado"),
+     *     @OA\Response(response=403, description="Proibido"),
+     * )
+     */
     public function consultarVendasRealizadas()
     {
         $vendas = Venda::all();
         return response()->json($vendas);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/vendas/{id}",
+     *     tags={"Produtos"},
+     *     summary="Obtém uma venda especifica",
+     *     @OA\Response(response=200, description="Operação executada com sucesso"),
+     *     @OA\Response(response=401, description="Não autenticado"),
+     *     @OA\Response(response=403, description="Proibido"),
+     * )
+     */
     public function consultarVendaEspecifica($id)
     {
         $venda = Venda::with('produtos')->find($id);
         return response()->json($venda);
     }
-
+    /**
+     * @OA\Delete(
+     *     path="/api/vendas/{id}",
+     *     tags={"Produtos"},
+     *     summary="Cancela uma venda especifica",
+     *     @OA\Parameter(
+     *         description="Id da venda",
+     *         in="path",
+     *         name="id",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Response(response=204, description="Operação executada com sucesso"),
+     *     @OA\Response(response=403, description="Proibido")
+     * )
+     */
     public function cancelarVenda($id)
     {
         $venda = Venda::find($id);
